071793d70204bb3dffdf6cc74d72eda56887f394
M <mihail.petuxov@yandex.ru>
2021-09-02 17:58:39 +0500

    Всё наладил

--- src/main/java/com/example/demoweb/controller/PostsViewController.java
+++ src/main/java/com/example/demoweb/controller/PostsViewController.java
@@ -1,6 +1,9 @@
 package com.example.demoweb.controller;
 
+import com.example.demoweb.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,6 +11,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PostsViewController {
+    @Autowired
+    PostService postsService;
+
     @ResponseBody
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String list() {
@@ -18,5 +24,11 @@ public class PostsViewController {
     @RequestMapping(path = "/post/{id}", method = RequestMethod.GET)
     public String single(@PathVariable("id") Long id) {
         return "Здесь будет страница поста №" + id;
+    }
+
+    @RequestMapping(path = "/abc", method = RequestMethod.GET)
+    public String list(Model model) {
+        model.addAttribute("posts", postsService.listAllPosts());
+        return "list";
     }
 }
--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -1,2 +1,16 @@
-package com.example.demoweb.service;public class PostService {
+package com.example.demoweb.service;
+
+import com.example.demoweb.model.Post;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+
+@Service
+public class PostService {
+    public ArrayList<Post> listAllPosts() {
+        ArrayList<Post> posts = new ArrayList<Post>();
+        posts.add(new Post("Один", 1));
+        posts.add(new Post("Два Два", 22));
+        posts.add(new Post("Три Три Три", 333));
+        return posts;
+    }
 }
