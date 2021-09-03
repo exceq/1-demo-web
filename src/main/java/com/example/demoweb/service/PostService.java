384241ad85b38b6ae672608038803f9341a38ba6
M <mihail.petuxov@yandex.ru>
2021-09-03 14:30:40 +0500

    Сервис для добавления поста

--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -2,16 +2,29 @@ package com.example.demoweb.service;
 
 import com.example.demoweb.model.Post;
 import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Date;
 
 @Service
 public class PostService {
+
+    static ArrayList<Post> posts = new ArrayList<Post>(createFirstPosts());
+
     public ArrayList<Post> listAllPosts() {
-        ArrayList<Post> posts = new ArrayList<Post>();
-        posts.add(new Post("Один", new Date()));
-        posts.add(new Post("Два Два", new Date()));
-        posts.add(new Post("Три Три Три", new Date()));
         return posts;
+    }
+
+    static Collection<Post> createFirstPosts() {
+        return Arrays.asList(
+                new Post("Один", new Date()),
+                new Post("Два Два", new Date()),
+                new Post("Три Три Три", new Date()));
+    }
+
+    public static void create(String text) {
+        posts.add(new Post(text, new Date()));
     }
 }
