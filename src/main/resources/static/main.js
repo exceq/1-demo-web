7d6938234c6beb73aa172b898b10a489d95a8179
M <mihail.petuxov@yandex.ru>
2021-09-05 20:35:08 +0500

    Взаимодействие с БД

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -2,7 +2,7 @@ window.onload = function() {
     $(".like").click((e) => {
         var btn = $(e.currentTarget);
         var postId = btn.attr("data-post-id");
-        btn.attr("disabled","disabled");
+        btn.attr("disabled", "disabled");
         $.post("/post/"+postId+"/like", (data) => {
             btn.text("" + data + " ❤");
             btn.removeClass("btn-secondary");
